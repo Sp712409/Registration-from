f4df6fb212fab55ca4e5c3ea31515028834d6036
Ramveer Singh <ramveersingharaon@gmail.com>
2022-10-08 00:02:59 +0530

    second:- how to serving public file and third video:- how to serving dynamic and partials file

--- src/app.js
+++ src/app.js
@@ -1,12 +1,34 @@
 const express =require("express");
-
 require("./db/conn");
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require("path");
+const hbs = require("hbs");
+
+//serving public file
+const public_path = path.join(__dirname,"../public");
+app.use(express.static(public_path));
+
+//serving dynamic file
+const dynamic_path = path.join(__dirname,"../templates/views");
+app.set("view engine","hbs");
+app.set("views", dynamic_path);
+
+
+//serving dynamic file
+const partials_path = path.join(__dirname,"../templates/partials");
+hbs.registerPartials(partials_path)
+
+
+
+
+
+
 
 app.get("/",(req,res)=>{
-    res.send("Hello Word")
+    res.render("home")
 });
+
 
 app.listen(port,()=>{
     console.log(`the server is running port no ${port}`);
